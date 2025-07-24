6da938fb77f75386edad07a646785812847558bc
ronnansants <ronnancardoso@hotmail.com>
2025-07-24 12:50:39 -0300

    Esse commit foi para colocar as Pages e o .gitignore

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function home() {
+  return (
+    <div>
+        <p>PAREI NA AULA DIA 06 - 23. Git push  </p>
+      <h1>Welcome to My Page</h1>
+
+      <p>This is a simple Next.js page.</p>
+    </div>
+  );
+}
+
+export default home;
